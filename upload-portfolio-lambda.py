8cc9fddbcb6909d0d73b5244392e288a5748b3c7
Thor Endre <thorendre@gmail.com>
2020-01-18 11:47:57 +0100

    Added upload portfolio script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,17 @@
+import boto3
+import StringIO
+import zipfile
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.thorendre.info')
+build_bucket = s3.Bucket('portfoliobuild.thorendre.info')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+       obj = myzip.open(nm)
+       portfolio_bucket.upload_fileobj(obj, nm)
+       portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
