3d8f9a2b6acdc584f8fa08303beabc62c93bb6a3
Thor Endre <thorendre@gmail.com>
2020-01-18 12:06:59 +0100

    Fixed lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,6 +1,7 @@
 import boto3
 import StringIO
 import zipfile
+import mimetypes
 
 s3 = boto3.resource('s3')
 
@@ -13,5 +14,6 @@ build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
        obj = myzip.open(nm)
-       portfolio_bucket.upload_fileobj(obj, nm)
+       portfolio_bucket.upload_fileobj(obj, nm,
+       ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
